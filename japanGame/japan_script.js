8d7628f7a3b2455641a55e8cce33994121511a80
mooglemagic <31009113+mooglemagic@users.noreply.github.com>
2024-06-14 20:11:39 +0100

    Updated Japan Game

--- japanGame/japan_script.js
+++ japanGame/japan_script.js
@@ -3,22 +3,29 @@
 // ctrl + spacebar
 
 let lives = 5;
+let englishVerbArray;
+let englishVerb;
 
 const verbs = {
-	"appear/come out": "でる",
-	"to know": "しる",
-	"to ride/ get on": "のる",
-	"fix/mend": "なおす",
-	"move operate": "うごく"
+	"To appear/come out": "でる",
+	"To know": "しる",
+	"To ride/ get on": "のる",
+	"To fix/mend": "なおす",
+	"To move operate": "うごく"
 }
 
-document.getElementById("test").textContent = verbs["appear/come out"];
+let questionLabel = document.getElementById("questionVerb");
 
-while (lives > 0) {
-	//game go
-	lives--;
+// document.getElementById("test").textContent = verbs["appear/come out"];
+
+function setRandomVerb() {
+	let index = Math.floor(Math.random()*englishVerbArray.length)
+	englishVerb = englishVerbArray[index];
 }
 
+function removeCurrentVerbFromArray() {
+	englishVerbArray.indexOf(englishVerb)
+}
 
 function checkVerbEnglishToJapanese(englishQuestion, japaneseAnswer) {
 	return verbs[englishQuestion] == japaneseAnswer;
@@ -29,6 +36,15 @@ function checkVerbJapaneseToEnglish(japaneseQuestion, englishAnswer) {
 	return true
 }
 
-console.log(
-	checkVerbEnglishToJapanese("to know", "しり")
-)
+
+function startGame() {
+	englishVerbArray = Object.keys(verbs);
+	setRandomVerb();
+	console.log(englishVerb);
+	questionLabel.innerHTML = englishVerb + ":"
+	// check if user has won
+	// check if user is out of lives
+	// ask next question
+}
+
+startGame()
