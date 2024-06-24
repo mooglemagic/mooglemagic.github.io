ade6c78772abc7dccfb42417f97a27bd0a3d2468
mooglemagic <31009113+mooglemagic@users.noreply.github.com>
2024-06-24 19:13:00 +0100

    Updated Japan App
    Player can no longer continue when all lives are spent.

--- japanGame/japan_script.js
+++ japanGame/japan_script.js
@@ -26,18 +26,24 @@ let submitButton = document.getElementById("submit");
 let answerInput = document.getElementById("answer");
 let livesHeader = document.getElementById("lives");
 let lifeContainer = document.getElementById("life-container");
+let gameOver = false;
 
 
 
 submitButton.onclick = ()=> {
-	let isAnswerCorrect = checkVerbEnglishToJapanese(englishVerb, answerInput.value);
-	if (isAnswerCorrect == false) {
-		reduceLife();
-		checkLife()
+	if(gameOver) {
+		reset();
 	} else {
-		removeCurrentVerbFromArray();
+		let isAnswerCorrect = checkVerbEnglishToJapanese(englishVerb, answerInput.value);
+		if (isAnswerCorrect == false) {
+			reduceLife();
+			checkLife();
+		} else {
+			removeCurrentVerbFromArray();
+		}
+		playRound();
 	}
-	playRound()
+
 };
 
 function setRandomVerb() {
@@ -70,14 +76,15 @@ function reduceLife() {
 	for(let i = 0; i < lives; i++) {
 		lifeContainer.innerHTML += '<img class="heart-img" src="imgs/heart.png">'
 	}
-	
 }
 
 function checkLife(){
 	if(lives<=0) {
 		//reset()
-		lifeContainer.innerHTML = '<h3 id="game-over">Game Over!</h3>'
-		console.log("DEAD");
+		lifeContainer.innerHTML = '<h3 id="game-over">Game Over!</h3>';
+		submitButton.value = "Play Again?";
+		answerInput.disabled = true;
+		gameOver = true;
 	}
 }
 
@@ -90,7 +97,26 @@ function playRound() {
 }
 
 function reset() {
-	//pass
+	words = {
+		"To appear/come out": "でる",
+		"To know": "しる",
+		"To ride/ get on": "のる",
+		"To fix/mend": "なおす",
+		"To move operate": "うごく",
+		"To laugh": "わらう",
+		"To be mad": "おこる",
+		"To cry": "なく",
+		"To lose":"なくす",
+		"To find (active)": "みつける",
+		"To find (passive)": "みつかる",
+		"Interview": "めんせつ"
+	}
+	// using reduceLife here to reinitialize lives
+	lives = 6;
+	reduceLife()
+	submitButton.value = "Go";
+	answerInput.disabled = false;
+	gameOver = false;
 }
 
 // starts game
